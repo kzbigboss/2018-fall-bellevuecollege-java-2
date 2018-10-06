5c56ee6a56312fa3f403e4c9f13f4a04e718b821
Mark Kazzaz <markkazzaz@gmail.com>
2018-10-05 22:51:20 -0700

    init ch10 files.  first draft of items

---
+++ chapter 10 assignment/src/Item.java
@@ -0,0 +1,49 @@
+public class Item {
+
+    // fields
+    String name;
+    double price;
+    int bulkQuantity;
+    double bulkPrice;
+
+    // constructors
+    public Item (String name, double price){
+        this(name, price, 0, 0);
+    }
+
+    public Item (String name, double price, int bulkQuantity, double bulkPrice){
+
+        // throw error if any value is less than zero
+        if (price < 0 || bulkPrice < 0 || bulkQuantity < 0){
+            throw new IllegalArgumentException("Numerical values cannot be less than zero.");
+        }
+
+        // assign class values
+        this.name = name;
+        this.price = price;
+        this.bulkQuantity = bulkQuantity;
+        this.bulkPrice = bulkPrice;
+    }
+
+    // instance methods
+    public double priceFor(int quantity){
+
+        // throw error if value is less than zero
+        if (quantity < 0){
+            throw new IllegalArgumentException("Quantity cannot be less than zero.");
+        }
+
+        // determine how many bulk quantities are being purchased
+        // then reduce quantity amount being fulfill via bulk quantities
+        int bulkInstance = quantity / bulkQuantity;
+        quantity -= bulkInstance * bulkQuantity;
+
+        // return the price
+        return  (quantity * price)              // individual quantities price
+                    +
+                (bulkInstance * bulkPrice);     // bulk quantities price
+
+    }
+
+
+}
