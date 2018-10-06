472625945af8a85767be14f0aa21f291a0a55a47
Mark Kazzaz <markkazzaz@gmail.com>
2018-10-06 16:55:24 -0700

    first pass of working ShoppingMain

--- chapter 10 assignment/src/ShoppingCart.java
+++ chapter 10 assignment/src/ShoppingCart.java
@@ -9,28 +9,46 @@ public class ShoppingCart {
 
     // fields
     private ArrayList<ItemOrder> listShoppingCart = new ArrayList<ItemOrder>(); // empty cart
-    private boolean discountApplied; // distcount defaults to false
+    private boolean discountApplied; // discount defaults to false
 
     public void add(ItemOrder inputItemOrder){
-        // TODO Check list and drop if item already exists
 
-        listShoppingCart.add(inputItemOrder);
+        int repeatIndex = -1;   // setup an index to figure out if the item is already in the cart
+
+        for (ItemOrder captured : listShoppingCart){    // scan through the cart
+            if (captured.getItem().equals(inputItemOrder.getItem())){   // determine if the cart already has the item
+                repeatIndex = listShoppingCart.indexOf(captured);       // if it does, update the index
+            }
+        }
+
+        if (repeatIndex != -1){
+            // if the index was updated then drop and add the item
+            listShoppingCart.remove(repeatIndex);
+            listShoppingCart.add(inputItemOrder);
+        } else {
+            // if the index wasn't updated, add the item
+            listShoppingCart.add(inputItemOrder);
+        }
     }
 
     // instance methods
     public double getTotal(){
-        double totalPrice = 0; // variable to capture running total
+        // variable to capture running total
+        double totalPrice = 0;
 
         // for each loop through the shopping card
         for (ItemOrder itemAdded : listShoppingCart){
-            totalPrice += itemAdded.getPrice(); // add individual ItemOrder to running total
+            // add individual ItemOrder to running total
+            totalPrice += itemAdded.getPrice();
         }
 
         // return total price
-        if (discountApplied){   // discount applied? then apply 10% discount
+        if (discountApplied){
+            // discount applied? then apply 10% discount
             return totalPrice * .9;
         } else {
-            return totalPrice;  // discount not applied?  return total amount without adjustment
+            // discount not applied?  return total amount without adjustment
+            return totalPrice;
         }
     }
 
