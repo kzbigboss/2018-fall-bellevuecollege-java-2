1c9de8566d0e7bbc9d3693cfa285a242d816d384
Mark Kazzaz <kazzazmk@amazon.com>
2018-11-15 21:57:34 -0800

    wrote static methods.  need to go back and redo.

--- ch15 assignment/src/ArrayIntList.java
+++ ch15 assignment/src/ArrayIntList.java
@@ -1,15 +1,22 @@
+/*
+Mark Kazzaz
+source class
+ */
+
+import java.util.*;
+
 // Class ArrayIntList can be used to store a list of integers.
 public class ArrayIntList implements Iterable<Integer> {
     private int[] elementData;  // list of integers
     private int size = 0;       // current number of elements in the list
-    
+
     public static final int DEFAULT_CAPACITY = 10;
-    
+
     // post: constructs an empty list of default capacity
     public ArrayIntList() {
         this(DEFAULT_CAPACITY);
     }
-    
+
     // pre : capacity >= 0
     // post: constructs an empty list with the given capacity
     private ArrayIntList(int capacity) {
@@ -18,7 +25,7 @@ public class ArrayIntList implements Iterable<Integer> {
         }
         elementData = new int[capacity];
     }
-    
+
     // for creating test cases more easily
     public ArrayIntList(int... elements) {
         this(Math.max(DEFAULT_CAPACITY, elements.length * 2));
@@ -26,13 +33,13 @@ public class ArrayIntList implements Iterable<Integer> {
             add(n);
         }
     }
-    
+
     // for creating test cases more easily
     public ArrayIntList(int element, boolean notCapacity) {
         this();
         add(element);
     }
-    
+
     // for creating test cases more easily
     public ArrayIntList(Collection<Integer> elements) {
         this(Math.max(DEFAULT_CAPACITY, elements.size() * 2));
@@ -40,50 +47,50 @@ public class ArrayIntList implements Iterable<Integer> {
             add(n);
         }
     }
-    
+
     // copy constructor; for creating test cases more easily
     public ArrayIntList(ArrayIntList list) {
         this(Math.max(DEFAULT_CAPACITY, list.size() * 2));
         addAll(list);
     }
-    
+
     // pre : size() < capacity (elementData.length)
     // post: appends the given value to the end of the list
     public void add(int value) {
         add(size, value);
     }
-    
+
     // pre: size() < capacity (elementData.length) && 0 <= index <= size()
     // post: inserts the given value at the given index, shifting subsequent
     //     values right
     public void add(int index, int value) {
         checkIndex(index, 0, size);
         ensureCapacity(size + 1);
-        
+
         for (int i = size; i > index; i--) {
             elementData[i] = elementData[i - 1];
         }
         elementData[index] = value;
         size++;
     }
-    
+
     // post: appends all values in the given list to the end of this list
     public void addAll(ArrayIntList other) {
         for (int i = 0; i < other.size; i++) {
             add(other.elementData[i]);
         }
     }
-    
+
     // post: list is empty
     public void clear() {
         size = 0;
-    } 
-    
+    }
+
     // post: returns true if the given value is contained in the list, false otherwise
     public boolean contains(int value) {
         return indexOf(value) != -1;
-    } 
-    
+    }
+
     // post: ensures that the underlying array has the given capacity; if not,
     // the size is doubled (or more if given capacity is even larger)
     public void ensureCapacity(int capacity) {
@@ -99,7 +106,7 @@ public class ArrayIntList implements Iterable<Integer> {
             elementData = newList;
         }
     }
-    
+
     // returns true if o is an ArrayIntList with the same size and elements as this one
     public boolean equals(Object o) {
         if (!(o instanceof ArrayIntList)) {
@@ -110,28 +117,28 @@ public class ArrayIntList implements Iterable<Integer> {
         if (this.size != other.size) {
             return false;
         }
-        
+
         for (int i = 0; i < size; i++) {
             if (elementData[i] != other.elementData[i]) {
                 return false;
             }
         }
-        
+
         return true;
     }
-    
+
     // pre : 0 <= index < size()
     // post: returns the integer at the given index in the list
     public int get(int index) {
         checkIndex(index);
         return elementData[index];
     }
-    
+
     // post: returns capacity of this list's underlying array
     public int getCapacity() {
         return elementData.length;
     }
-    
+
     // post : returns the position of the first occurence of the given
     //      value (-1 if not found)
     public int indexOf(int value) {
@@ -142,17 +149,17 @@ public class ArrayIntList implements Iterable<Integer> {
         }
         return -1;
     }
-    
+
     // post: returns true if list is empty, false otherwise
     public boolean isEmpty() {
         return size == 0;
     }
-    
+
     // post: returns an iterator for this list
     public Iterator<Integer> iterator() {
         return new ArrayIntListIterator(this);
-    } 
-    
+    }
+
     // pre : 0 <= index < size()
     // post: removes value at the given index, shifting subsequent values left
     public void remove(int index) {
@@ -162,7 +169,7 @@ public class ArrayIntList implements Iterable<Integer> {
         }
         size--;
     }
-    
+
     // post: removes all occurrences of the values in the given list from this list
     public void removeAll(ArrayIntList other) {
         int newSize = 0;
@@ -174,24 +181,24 @@ public class ArrayIntList implements Iterable<Integer> {
             size = newSize;
         }
     }
-    
+
     // pre : 0 <= index < size()
     // post: replaces the integer at the given index with the given value
     public void set(int index, int value) {
         checkIndex(index);
         elementData[index] = value;
-    } 
-    
+    }
+
     // post: returns the current number of elements in the list
     public int size() {
         return size;
     }
-    
+
     // post: returns an array version of the contents of this list
     public int[] toArray() {
         return Arrays.copyOf(elementData, size);
     }
-    
+
     // post: creates a comma-separated, bracketed version of the list
     public String toString() {
         String result = "[";
@@ -209,33 +216,33 @@ public class ArrayIntList implements Iterable<Integer> {
         result += "]";
         return result;
     }
-    
+
     // helpers to make sure indexes do not fall out of bounds
     private void checkIndex(int index) {
         checkIndex(index, 0, size - 1);
     }
-    
+
     private void checkIndex(int index, int min, int max) {
         if (!(min <= index && index <= max)) {
-            throw new ArrayIndexOutOfBoundsException("Illegal index " + index + 
+            throw new ArrayIndexOutOfBoundsException("Illegal index " + index +
                     "; must be between " + min + " and " + max + "\n"
                     + "list : " + toString() + " (size=" + size + ")\n"
                     + "array: " + Arrays.toString(elementData) + " (capacity=" + elementData.length + ")");
         }
     }
-    
-    
+
+
     // Stuart Reges
     // 4/4/05
     //
     // The ArrayIntListIterator class provides a set of utilities for iterating
     // over an ArrayIntList and possibly removing values from the list.
-    
+
     private static class ArrayIntListIterator implements Iterator<Integer> {
         private ArrayIntList list;    // list to iterate over
         private int position;          // current position within the list
         private boolean removeOK;      // whether it's okay to remove now
-        
+
         // pre : list != null
         // post: constructs an iterator for the given list
         public ArrayIntListIterator(ArrayIntList list) {
@@ -243,12 +250,12 @@ public class ArrayIntList implements Iterable<Integer> {
             position = 0;
             removeOK = false;
         }
-        
+
         // post: returns true if there are more elements left, false otherwise
         public boolean hasNext() {
             return position < list.size();
         }
-        
+
         // pre : hasNext()
         // post: returns the next element in the iteration
         public Integer next() {
@@ -259,7 +266,7 @@ public class ArrayIntList implements Iterable<Integer> {
             removeOK = true;
             return result;
         }
-        
+
         // pre : next() has been called without a call on remove (i.e., at most one
         //     call per call on next)
         // post: removes the last element returned by the iterator
@@ -270,8 +277,88 @@ public class ArrayIntList implements Iterable<Integer> {
             position--;
             removeOK = false;
         }
-    }    
+    }
 
-// YOUR CODE GOES HERE
+    // start of code written by Mark Kazzaz
+
+    // requirement 1: method called uptoNowTotal() that returns a
+    // new ArrayIntList containing a running total of the input list
+
+    // post:    returns a newly constructed ArrayIntList reflecting a
+    //          running total of the input list.
+    //          &&
+    //          newly created ArrayIntList has the same capacity value.
+
+    public static ArrayIntList upToNowTotal(ArrayIntList input) {
+        // TODO do not make these static
+
+        // create new ArrayIntList with same capacity as input list
+        ArrayIntList result = new ArrayIntList(input.getCapacity());
+
+        // evaluate if size is < 0
+        if (input.size() < 0) {
+            // if true, return empty list
+            return result;
+        } else {
+            // if false, proceed with method
+
+            // fencepost loop through the input list while
+            // adding the previous result list and the current input
+            // list element values together
+
+            /// fencepost
+            result.add(input.get(0));
+
+            /// current input element + prior result element
+            for (int i = 1; i < input.size(); i++) { // loop 1 thru N
+                result.add(input.get(i) + result.get(i - 1));
+            }
+
+            return result;
+        }
+
+    }
+
+    // requirement 2: method called isPairSorted() that returns a boolean
+    // indicating if the pairs in a ArrayIntList are in sorted order. if a
+    // list contains an odd number of elements, ignore the final element. if a
+    // list contains zero elements, report true.
+
+    // post:    function steps through the pairs of elements in the input list
+    //          and reports true/false based on whether the pairs are sorted.
+
+    public static boolean isPairSorted(ArrayIntList input) {
+
+        // we can use a for loop based on half the size of the list size.  This
+        // will help us compare pairs as well as ignore the final element in
+        // odd sized lists.
+
+        for (int i = 0; i < input.size() / 2; i++) {
+            // leverage multiplication and figure out if all pairs are ordered.
+            // if not, return false
+            int firstHalf = input.get(i * 2);
+            int secondHalf = input.get(i * 2 + 1);
+            if (firstHalf > secondHalf) {
+                return false;
+            }
+
+        }
+
+        // if we traverse the list without setting off the if statement above,
+        // then return true
+        return true;
+    }
+
+    // requirement 3: method called removeLast() that takes int n as a parameter
+    // and removed the first n values from a list of integers
+
+
+    public void removeLast(int n){
+        int start = size - n;
+
+        for (int i = start; i < size; i++){
+
+        }
+    }
 
 }
--- ch15 assignment/src/Assignment7.java
+++ ch15 assignment/src/Assignment7.java
@@ -1,2 +1,105 @@
+/*
+Mark Kazzaz
+test class
+ */
+
 public class Assignment7 {
+    public static void main(String[] args) {
+
+        //testProblem1();
+        testProblem2();
+
+    }
+
+    public static void testProblem1() {
+        // Testing Problem 1: upToNowTotal() running total method
+        System.out.println("______START PROBLEM 1 TESTING______");
+        System.out.println();
+
+        /// create array for problem1a
+        ArrayIntList problem1a = new ArrayIntList();
+
+        /// add elements to problem1a array
+        problem1a.add(2);
+        problem1a.add(3);
+        problem1a.add(5);
+        problem1a.add(4);
+        problem1a.add(7);
+        problem1a.add(15);
+        problem1a.add(20);
+        problem1a.add(7);
+
+        /// print to screen problem1a array
+        System.out.print("Starting array: ");
+        System.out.println(problem1a);
+
+        /// manually print expected array
+        /// TODO
+
+        /// involve running total method via Problem 1
+        ArrayIntList problem1b = ArrayIntList.upToNowTotal(problem1a);
+
+        /// print to screen problem1a array
+        System.out.print("Resulting array:");
+        System.out.println(problem1b);
+
+        /// double check that capacities match
+        boolean matchingCapacities = problem1a.getCapacity() == problem1b.getCapacity();
+        System.out.println("Capacities match: " + matchingCapacities);
+
+        /// end of problem 1 testing
+        System.out.println();
+        System.out.println("______FINISH PROBLEM 1 TESTING______");
+        System.out.println();
+    }
+
+    public static void testProblem2() {
+
+//        ArrayIntList problem2Sorted = new ArrayIntList();
+//        problem2Sorted.add(3);
+//        problem2Sorted.add(8);
+//        problem2Sorted.add(2);
+//        problem2Sorted.add(5);
+//        problem2Sorted.add(19);
+//        problem2Sorted.add(24);
+//        problem2Sorted.add(-3);
+//        problem2Sorted.add(0);
+//        problem2Sorted.add(4);
+//        problem2Sorted.add(4);
+//        problem2Sorted.add(8);
+//        problem2Sorted.add(205);
+//        problem2Sorted.add(42);
+//
+//        System.out.println(problem2Sorted);
+//
+//        System.out.println(ArrayIntList.isPairSorted(problem2Sorted));
+
+        ArrayIntList problem2Unsorted = new ArrayIntList();
+        problem2Unsorted.add(1);
+        problem2Unsorted.add(9);
+        problem2Unsorted.add(3);
+        problem2Unsorted.add(17);
+        problem2Unsorted.add(4);
+        problem2Unsorted.add(28);
+        problem2Unsorted.add(-5);
+        problem2Unsorted.add(-3);
+        problem2Unsorted.add(0);
+        problem2Unsorted.add(42);
+        problem2Unsorted.add(308);
+        problem2Unsorted.add(409);
+        problem2Unsorted.add(19);
+        problem2Unsorted.add(17);
+        problem2Unsorted.add(2);
+        problem2Unsorted.add(4);
+
+        System.out.println(problem2Unsorted);
+
+        System.out.println(ArrayIntList.isPairSorted(problem2Unsorted));
+
+
+    }
+
+    public static void testProblem3() {
+
+    }
 }
